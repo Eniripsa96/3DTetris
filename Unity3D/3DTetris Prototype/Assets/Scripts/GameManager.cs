2233fd10a6f3d1665e2358d3c1dc50213741b25a
unknown <twb1193@GDD-60.main.ad.rit.edu>
2014-09-25 15:55:22 -0400

    Added start of Unity project for the prototype. It has one script (GameManager) and one scene that has a camera and a gameManager.

---
+++ Unity3D/3DTetris Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	GameObject[,] gameGrid;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameGrid = new GameObject[10,18];	
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool isOccupied(int x, int y) 
+	{
+		return gameGrid[x,y] != null;
+	}
+
+	public void set(int x, int y, GameObject go)
+	{
+		gameGrid[x,y] = go;
+	}
+}
+
+
