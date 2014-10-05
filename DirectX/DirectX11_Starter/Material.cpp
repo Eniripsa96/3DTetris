a38fccdc6b9f59fdb79e6cd5ffb4e92e5bc88542
Thomas Bentley <twb1193@rit.edu>
2014-10-05 10:50:26 -0400

    Comments.

--- DirectX/DirectX11_Starter/Material.cpp
+++ DirectX/DirectX11_Starter/Material.cpp
@@ -2,20 +2,24 @@
 
 Material::Material(ID3D11Device* device, ID3D11DeviceContext* context, ID3D11VertexShader* vShader, ID3D11PixelShader* pShader, const wchar_t* texPath)
 {
+	// Set dx11 variables
 	deviceContext = context;
 	vertexShader = vShader;
 	pixelShader = pShader;
 
-	device->CreateShaderResourceView(NULL , NULL, &resourceView);
+	// Create SRV
+	HRESULT result1 = device->CreateShaderResourceView(NULL , NULL, &resourceView);	// Error here: Cannot create an SRV from a null resource (first param)
+
+	// Create sampler description
 	D3D11_SAMPLER_DESC* samplerDesc = new D3D11_SAMPLER_DESC();
 	samplerDesc->Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 	samplerDesc->AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc->AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc->AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	HRESULT result2 = device->CreateSamplerState(samplerDesc, &samplerState);
 
-	HRESULT result1 = device->CreateSamplerState(samplerDesc, &samplerState);
-
-	HRESULT result2 = CreateWICTextureFromFile(device, context, texPath, 0, &resourceView);
+	// Create texture
+	HRESULT result3 = CreateWICTextureFromFile(device, context, texPath, 0, &resourceView);
 
 	delete samplerDesc;
 }
