87e35adf571d4bb57df1a10a2835c6c719f8c3af
Thomas Bentley <twb1193@rit.edu>
2014-10-28 10:41:22 -0400

    Continued working on camera rotation.

--- DirectX/DirectX11_Starter/Camera.cpp
+++ DirectX/DirectX11_Starter/Camera.cpp
@@ -1,18 +1,21 @@
 #include "Camera.h"
 
+#include <iostream>;
+
+using namespace std;
 
 Camera::Camera()
 {
 	// Set up view matrix (camera)
 	// In an actual game, update this when the camera moves (every frame)
 	position = XMVectorSet(0, 0, -5, 0);
-	target = XMVectorSet(0, 0, 0, 0);
+	target = XMVectorSet(0, 0, 10, 0);
 	up = XMVectorSet(0, 1, 0, 0);
 	XMMATRIX V = XMMatrixLookAtLH(position, target, up);
 	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V));
 
 	myPosition = XMFLOAT4(0, 0, -5, 0);
-	myTarget = XMFLOAT4(0, 0, 0, 0);
+	myTarget = XMFLOAT4(0, 0, 10, 0);
 
 	projectionMatrix = XMFLOAT4X4();
 }
@@ -34,9 +37,21 @@ void Camera::Move(XMFLOAT3* move)
 
 void Camera::Rotate(XMFLOAT3* rotate)
 {
-	myPosition.x += rotate->x / 10.0f;
-	myPosition.y += rotate->y / 10.0f;
-	myPosition.z += rotate->z / 10.0f;
+	XMFLOAT4 up = XMFLOAT4(0.0f, 1.0f, 0.0f, 0.0f);
+	XMVECTOR UP = XMLoadFloat4(&up);
+
+	myTarget.x += -1.0f * rotate->x / 5.0f;
+	myTarget.y += -1.0f * rotate->y / 5.0f;
+	myTarget.z += -1.0f * rotate->z / 5.0f;
+
+	XMVECTOR vector = XMVECTOR();
+
+	/*XMFLOAT4 newTarget = myTarget;
+	vector = XMLoadFloat4(&newTarget);
+	vector = XMVector3Rotate(vector, XMQuaternionRotationMatrix(XMMatrixRotationAxis(UP, -1.0f * rotate->x * XM_PI / 32)));
+	vector += target;*/
+
+	//XMStoreFloat4(&myTarget, vector);
 }
 
 void Camera::Update(float dt)
@@ -46,4 +61,5 @@ void Camera::Update(float dt)
 	XMMATRIX V = XMMatrixLookAtLH(position, target, up);
 	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V));
 	//XMMatrixPerspectiveFovLH
+	//	XMMatrixPerspectiveFovLH
 }
