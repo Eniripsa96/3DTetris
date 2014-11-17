3c8c0205bb2fe93147ebbbc768a61b0164b71423
Thomas Bentley <twb1193@rit.edu>
2014-11-16 20:34:40 -0500

    Camera movement should stay correct now.

--- DirectX/DirectX11_Starter/Camera.cpp
+++ DirectX/DirectX11_Starter/Camera.cpp
@@ -1,6 +1,5 @@
 #include "Camera.h"
-
-#include <iostream>;
+#include <iostream>
 
 using namespace std;
 
@@ -105,15 +104,65 @@ void Camera::RotateY(float angle)
 	XMStoreFloat3(&target, XMVector3TransformNormal(XMLoadFloat3(&target), R));
 }
 
+// Rebuild view matrix
 void Camera::Update(float dt)
 {
-	// Rebuild view matrix
-	//position = XMVectorSet(myPosition.x, myPosition.y, myPosition.z, 0.0f);
-	//target = XMVectorSet(target.x, target.y, target.z, 0.0f);
-	XMMATRIX V = XMMatrixLookAtLH(XMLoadFloat3(&position), XMLoadFloat3(&target), GetUpXM());
-	XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V));
+	XMVECTOR R = XMLoadFloat3(&right);
+	XMVECTOR U = XMLoadFloat3(&up);
+	XMVECTOR F = XMLoadFloat3(&forward);
+	XMVECTOR P = XMLoadFloat3(&position);
 
-	// TODO: Renormalize basis vectors
+	// Orthonormalize the right, up, and look vectors
+
+	// Normalize forward to unit length
+	F = XMVector3Normalize(F);
+
+	// Compute new up vector and normalize
+	U = XMVector3Normalize(XMVector3Cross(F, R));
+
+	// Compute new right vector. U and F are already ortho-normal so no need to normalize the cross
+	// ||up x forward|| = ||up|| ||forward|| sin90 = 1
+	R = XMVector3Cross(U, F);
+
+	// Fill in the view matrix entries
+	float x = -XMVectorGetX(XMVector3Dot(P, R));
+	float y = -XMVectorGetX(XMVector3Dot(P, U));
+	float z = -XMVectorGetX(XMVector3Dot(P, F));
+
+	XMStoreFloat3(&right, R);
+	XMStoreFloat3(&up, U);
+	XMStoreFloat3(&forward, F);
+
+	XMStoreFloat3(&right, R);
+	XMStoreFloat3(&up, U);
+	XMStoreFloat3(&forward, F);
+
+	XMFLOAT4X4 temp;
+	temp(0, 0) = right.x;
+	temp(0, 1) = right.y;
+	temp(0, 2) = right.z;
+	temp(0, 3) = x;
+	
+	temp(1, 0) = up.x;
+	temp(1, 1) = up.y;
+	temp(1, 2) = up.z;
+	temp(1, 3) = y;
+	
+	temp(2, 0) = forward.x;
+	temp(2, 1) = forward.y;
+	temp(2, 2) = forward.z;
+	temp(2, 3) = z;
+		 
+	temp(3, 0) = 0.0f;
+	temp(3, 1) = 0.0f;
+	temp(3, 2) = 0.0f;
+	temp(3, 3) = 1.0f;
+
+	viewMatrix = temp;
+
+	// Recreate view matrix
+	//XMMATRIX V = XMMatrixLookAtLH(XMLoadFloat3(&position), XMLoadFloat3(&target), GetUpXM());
+	//XMStoreFloat4x4(&viewMatrix, XMMatrixTranspose(V));
 }
 
 #pragma region Accessors
